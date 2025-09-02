cf38b55f04e2cf9bdfdc3afa8e1f077e0c36e667
Erin Geier <erin.geier@gmail.com>
2025-09-02 14:32:17 -0500

    test dua function and branch deployment

--- src/amplifyClient.ts
+++ src/amplifyClient.ts
@@ -1,5 +1,5 @@
-// src/amplifyClient.ts
-import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.prod.json';
+// // src/amplifyClient.ts
+// import { Amplify } from 'aws-amplify';
+// import outputs from '../amplify_outputs.prod.json';
 
-Amplify.configure(outputs, { ssr: true }); // ssr:true is harmless in SPA, need for Next.js
+// Amplify.configure(outputs, { ssr: true }); // ssr:true is harmless in SPA, need for Next.js
