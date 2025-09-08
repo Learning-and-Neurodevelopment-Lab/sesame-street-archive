ccafc7a93f8ce62bcb9c69c0e67ad842b96161b0
antcoop-vu <anthony.d.cooper@vanderbilt.edu>
2025-09-08 14:53:49 -0500

    added basic jest and storybook component coverage

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/out/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/components', '<rootDir>/lib', '<rootDir>/__tests__'],
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
+
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+export { sum } from "./sum";
 
 export function cn(...classes: string[]) {
   return twMerge(clsx(classes));
