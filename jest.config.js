fe2129acb2e2e7e643c7387bbce8013b8392b21f
Erin Geier <erin.geier@gmail.com>
2025-09-15 13:35:42 -0500

    partial import

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,12 @@
-const nextJest = require("next/jest");
+import nextJest from "next/jest";
 const createJestConfig = nextJest({ dir: "./" });
 
-module.exports = async () => ({
+export default async () => ({
   ...(await createJestConfig({
     testEnvironment: "jsdom",
     rootDir: "",
   })()),
   transformIgnorePatterns: ['node_modules/(?!next-intl)/'],
-  // preset: "ts-jest",
-  // testEnvironment: "jsdom",
-  // testPathIgnorePatterns: [
-  //   "/node_modules/",
-  //   "/.next/",
-  //   "/out/",
-  //   "node_modules/(?!next-intl)/",
-  // ],
-  // moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  // roots: ["<rootDir>/components", "<rootDir>/lib", "<rootDir>/__tests__"],
-  // transform: {
-  //   "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-  // },
-  // moduleNameMapper: {
-  //   "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  //   "^@/(.*)$": "<rootDir>/$1",
-  //   "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-  //     "<rootDir>/__mocks__/fileMock.js",
-  // },
+ 
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
 });
