b80ba03297ce151773c88aadbebbcabcbbdb0617
antcoop-vu <anthony.d.cooper@vanderbilt.edu>
2025-09-16 00:02:33 -0500

    updated configuration and package lock

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,17 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "react/no-unescaped-entities": 0,
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
+  {
     ignores: [
       "node_modules/**",
       ".next/**",
