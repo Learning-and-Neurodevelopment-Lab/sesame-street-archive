e0c4126d84ac8c67e88c9c9dbdc7d20a8460f44f
Erin Geier <erin.geier@gmail.com>
2025-09-17 18:12:50 -0500

    add storage permissions for cognito groups

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -73,7 +73,7 @@ Annotation: a
     
     .authorization((allow) => [
       allow.owner(), // the signed-in owner can CRUD their own profile
-      allow.groups(["ADMIN"]).to(["read", "update"]), // admins can help edit
+      allow.groups(["ADMIN"]).to(["read", "update","delete","get"]), // admins can help edit
     ]),
     // Minimal, stable user profile keyed by Cognito sub.
   DuaUsers: a
@@ -97,7 +97,7 @@ Annotation: a
     .identifier(["userSub"])
     .authorization((allow) => [
       allow.owner(), // the signed-in owner can CRUD their own profile
-      allow.groups(["ADMIN"]).to(["read", "update"]), // admins can help edit
+      allow.groups(["ADMIN"]).to(["read", "update", "delete"]), // admins can help edit
     ]),
   // Append-only DUA acceptance history (minimal)
   DuaConsent: a
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,20 +4,28 @@ export const storage = defineStorage({
   name: 'sesame-street-images',
   access: (allow) => ({
     'dev/*': [
-      allow.authenticated.to(['read','write']),
-      allow.guest.to(['read'])
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),          // all signed-in users can read
+      allow.groups(["RESEARCHER", "USER"]).to(["read"]), 
+      allow.groups(["ADMIN"]).to(["read", "write"]), // admins can help edit
     ],
     'input/*': [
-      allow.authenticated.to(['read','write']),
-      allow.guest.to(['read','write'])
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),          // all signed-in users can read
+      allow.groups(["RESEARCHER", "USER"]).to(["read"]), 
+      allow.groups(["ADMIN"]).to(["read", "write"]), // admins can help edit
     ],
     'data/*': [
-      allow.authenticated.to(['read','write']),
-      allow.guest.to(['read'])
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),          // all signed-in users can read
+      allow.groups(["RESEARCHER", "USER"]).to(["read"]), 
+      allow.groups(["ADMIN"]).to(["read", "write"]), // admins can help edit
     ],
     'images/*': [
-      allow.authenticated.to(['read','write']),
-      allow.guest.to(['read'])
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read']),          // all signed-in users can read this makes the USER group redundant
+      allow.groups(["RESEARCHER", "USER"]).to(["read"]), 
+      allow.groups(["ADMIN"]).to(["read", "write"]), // admins can help edit
     ]
   })
 });
