4df6b2cfb059c479b33050ec8f3198332fa18509
Erin Geier <erin.geier@gmail.com>
2025-09-17 15:01:03 -0500

    add types

--- src/amplifyClient.ts
+++ src/amplifyClient.ts
@@ -1,5 +1,5 @@
 // src/amplifyClient.ts
 import { Amplify } from 'aws-amplify';
-import outputs from '../amplify_outputs.prod.json';
+import outputs from '../amplify_outputs.json';
 
 Amplify.configure(outputs, { ssr: true }); // ssr:true is harmless in SPA, need for Next.js
