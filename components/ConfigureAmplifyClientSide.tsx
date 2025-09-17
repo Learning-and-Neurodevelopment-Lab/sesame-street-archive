e1511ea91131d0b2f96ecee92a504cbb61215da5
antcoop-vu <anthony.d.cooper@vanderbilt.edu>
2025-09-17 12:59:29 -0500

    reverted amplify

--- components/ConfigureAmplifyClientSide.tsx
+++ components/ConfigureAmplifyClientSide.tsx
@@ -1,12 +1,11 @@
 'use client';
+
 import { Amplify } from 'aws-amplify';
 import outputs from '@/amplify_outputs.json';
-import { setupUserPersist } from '@/app/(main)/auth/signin/userPersist';
 
 import type { ResourcesConfig } from 'aws-amplify';
 
 Amplify.configure(outputs as ResourcesConfig, { ssr: true });
-setupUserPersist();
 
 export default function ConfigureAmplifyClientSide() {
   return null;
