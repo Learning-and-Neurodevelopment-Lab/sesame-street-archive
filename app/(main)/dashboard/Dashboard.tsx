8409f118c63ead2ac32b25f88eaab4a539100cfa
antcoop-vu <anthony.d.cooper@vanderbilt.edu>
2025-09-18 06:44:30 -0500

    expanded sample set for dashboard

--- app/(main)/dashboard/Dashboard.tsx
+++ app/(main)/dashboard/Dashboard.tsx
@@ -50,27 +50,33 @@ export default function Dashboard() {
     queryFn: async () => {
       if (!isAuthenticated) return [];
 
+      // S48-E4831_00071
+      // S48-E4831_00655
+      // S48-E4829_00131
+      // S48-E4831_01156
+      // S36-E4086_02183
+
       const { data } = await client.models.Image.list({
         filter: {
           or: [
             { image_id: "00751", episode_id: "4274", season: 42 },
             { image_id: "00071", episode_id: "4831", season: 48 },
+            { image_id: "00655", episode_id: "4831", season: 48 },
+            { image_id: "00131", episode_id: "4829", season: 48 },
             { image_id: "01156", episode_id: "4831", season: 48 },
-            { image_id: "00469", episode_id: "4817", season: 48 },
-            { image_id: "00266", episode_id: "4109", season: 48 },
-            { image_id: "00344", episode_id: "4605", season: 48 },
+            { image_id: "00222", episode_id: "4829", season: 48 },
           ].map(({ image_id, episode_id, season }) => ({
             image_id: { eq: image_id },
             episode_id: { eq: episode_id },
             season: { eq: season },
           })),
         } as any,
-        limit: 6,
       });
       return data || [];
     },
     staleTime: 1000 * 60 * 5,
     refetchOnMount: true,
+    enabled: isAuthenticated,
   });
 
   const imageIds = useMemo(
@@ -96,6 +102,7 @@ export default function Dashboard() {
     },
     staleTime: 1000 * 60 * 5,
     refetchOnMount: true,
+    enabled: isAuthenticated && images.length > 0,
   });
 
   let imagesWithAnnotations = [];
@@ -180,56 +187,72 @@ export default function Dashboard() {
   }
 
   return (
-    <div className="grid min-h-screen bg-gray-50 flex-wrap sm:grid-cols-[1fr_auto]">
+    <div className="grid min-h-screen bg-gray-50 flex-wrap sm:grid-cols-[auto_1fr] max-w-8xl">
       {/* Aside Navigation */}
       <aside className="w-full sm:w-64 bg-white border-r border-gray-200 flex flex-col py-8 px-6">
-        <h2 className="text-xl font-bold mb-8">Dashboard</h2>
+        <h2 className="text-xl font-medium mb-8">Dashboard</h2>
         <nav className="flex flex-col gap-2">
-          <Link href="#" className="py-1 font-normal text-gray-800  cursor-pointer">Collections</Link>
-          <span className="py-1 text-gray-400 cursor-not-allowed">Contributions</span>
-          <span className="py-1 text-gray-400 cursor-not-allowed">Scoreboard</span>
-          <span className="py-1 text-gray-400 cursor-not-allowed">Suggested</span>
-          <span className="py-1 text-gray-400 cursor-not-allowed">Notifications</span>
+          <Link
+            href="#"
+            className="py-1 font-normal text-gray-800  cursor-pointer"
+          >
+            Collections
+          </Link>
+          <span className="py-1 text-gray-400 cursor-not-allowed">
+            Contributions
+          </span>
+          <span className="py-1 text-gray-400 cursor-not-allowed">
+            Scoreboard
+          </span>
+          <span className="py-1 text-gray-400 cursor-not-allowed">
+            Suggested
+          </span>
+          <span className="py-1 text-gray-400 cursor-not-allowed">
+            Notifications
+          </span>
           <span className="py-1 text-gray-400 cursor-not-allowed">Account</span>
         </nav>
       </aside>
       {/* Main Content */}
-      <main className="flex-1 max-w-5xl mx-auto py-4 px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {imagesWithAnnotations.map((result) => {
-            const imgUrl = imageUrls[result.id] || "";
-            return (
-              <Link
-                href={`/annotate?image=${encodeURIComponent(imgUrl)}`}
-                key={result.imagePath}
-                className="rounded-xl overflow-hidden shadow hover:shadow-lg transition-shadow bg-white flex flex-col items-center"
-              >
-                {imgUrl ? (
-                  <FadeInImage
-                    key={`${result.id}-${result.episode}-image`}
-                    src={imgUrl}
-                    alt={String(result.id)}
-                    width={100}
-                    height={100}
-                    className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300 select-none pointer-events-none"
-                    loading="lazy"
-                    draggable="false"
-                  />
-                ) : (
-                  <div className="w-full h-full flex items-center justify-center text-gray-400">
-                    <PulseLoader color="#9ca3af" size={8} />
+      <main className="flex-1 py-4 px-4 justify-self-center">
+        <div className="max-w-7xl mx-auto px-4 py-4">
+          <div className="mb-8">
+            <h1 className="text-2xl font-medium mb-4">Annotation Collection</h1>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {imagesWithAnnotations.map((result) => {
+              const imgUrl = imageUrls[result.id] || "";
+              return (
+                <Link
+                  href={`/annotate?image=${encodeURIComponent(imgUrl)}`}
+                  key={result.imagePath}
+                  className="rounded-xl overflow-hidden shadow hover:shadow-lg transition-shadow bg-white flex flex-col items-center"
+                >
+                  {imgUrl ? (
+                    <FadeInImage
+                      key={`${result.id}-${result.episode}-image`}
+                      src={imgUrl}
+                      alt={String(result.id)}
+                      width={100}
+                      height={100}
+                      className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300 select-none pointer-events-none"
+                      loading="lazy"
+                      draggable="false"
+                    />
+                  ) : (
+                    <div className="w-full h-full flex items-center justify-center text-gray-400">
+                      <PulseLoader color="#9ca3af" size={8} />
+                    </div>
+                  )}
+                  <div className="p-4 w-full flex justify-center">
+                    <div className="inline-block bg-gray-600 hover:bg-gray-700 text-white font-semibold px-4 py-2 rounded transition">
+                      Annotate
+                    </div>
                   </div>
-                )}
-                <div className="p-4 w-full flex justify-center">
-                  <div
-                    className="inline-block bg-gray-600 hover:bg-gray-700 text-white font-semibold px-4 py-2 rounded transition"
-                  >
-                    Annotate
-                  </div>
-                </div>
-              </Link>
-            );
-          })}
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </main>
     </div>
